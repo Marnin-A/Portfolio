1073e68f4e4806ae5993c266f85f5b9c87bedae3
Marnin Audu <msmaudu@gmail.com>
2023-01-19 11:48:14 +0100

    Added file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { About, Hero, Projects } from "./sections";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hero />
+      {/* This should have a smooth welcoming animation */}
+
+      <Projects />
+      {/* Each project will have:
+      1. A gif to the left that shows a live deployment
+      2. Text to the right that explains the reasoning behind it
+      3. ...
+      */}
+
+      <About />
+      {/* Concise information about me, and links to my social media */}
+    </div>
+  );
 }
 
 export default App;
